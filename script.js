f4f2b49c00f68eeb79065658d35fcb37629e28fd
SyenLxyz <syenlxyz@gmail.com>
2022-08-25 10:09:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let input = document.getElementById('input');
+input.addEventListener('keyup', function(event) {
+  let output = document.getElementById('output');
+  output.contentDocument.open();
+  output.contentDocument.write(input.value);
+  output.contentDocument.close();
+});
