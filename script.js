cb5689e9f58474eb3bb3902f9cb86ee6bcc3e3b2
SyenLxyz <syenlxyz@gmail.com>
2022-08-25 16:43:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,63 +4,81 @@ let metadata = {
   'key': null
 }
 
-let data  = [];
+let content = [];
 
-let input = document.getElementById('input');
- 
-let sync = function(event) {
-  let text = input.value;
-  let lines = text.split('\n');
-
-  lines.map(function(line) {
-    let pattern = /{(.*?)}/;
-    let result = pattern.exec(line);
-    if (result) {
-      let directive = result[1]
-      if (directive.includes(':')) {
-        let key = directive.split(':')[0].trim();
-        let value = directive.split(':')[1].trim();
-
-        let keys = Object.keys(metadata);
-        if (keys.includes(key)) {
-          metadata[key] = value;
-        } else {
-          let item = {[key]: value}; 
-          data.push(item);
-        }
-      } else {
-        ;
-      }
-    } else {
-      line = line.trim()
-      if (!line) {
-        data.push(line);
-      } else {
-        getItem(line)
-      }
-      
-    }
-  });
-
-  console.log(data);
-
-  /*
-  let output = document.getElementById('output');
-  output.contentDocument.open();
-  output.contentDocument.write(data);
-  output.contentDocument.close();
-  */
-}
-
-let getItem = function(line) {
+function getLine(line) {
   let chord = [];
   let lyric = [];
-  
+
   while (line) {
     let pattern = /[(.*?)]/;
     let result = pattern.exec(line);
     
+    if (result && result.index == 0) {
+      chord.push(result[1]);
+      line = line.replace(match[0], '');
+    } else {
+      chord.push('');
+    }
+
+    if (line) {
+      lyric.push(line[0]);
+      line = line.slice(1);
+    } else {
+      lyric.push('');
+    }
+  }
+
+  item = {
+    'chord': chord, 
+    'lyric': lyric
+  }
+  return item;
+}
+
+function getLines(line) {
+  let pattern = /(.*?)/;
+  let result = pattern.exec(line);
+
+  if (result) {
+    let directive = result[1];
+    if (directive.includes(':')) {
+      let key = directive.split(':')[0].trim();
+      let value = directive.split(':')[1].trim();
+
+      let metaKeys = Object.keys(metadata)
+      if (metaKeys.includes(key)) {
+        metadata[key] = value;
+      } else if (key == 'section') {
+        let item = {[key]: value};
+        content.push(item);
+      }
+    }
+  } else {
+    line = line.trim();
+    if (!line) {
+      content.push(line);
+    } else {
+      let item = getLine(line);
+      content.push(item);
+    }
   }
 }
 
-input.addEventListener('keyup', sync);
+function runCode() {
+  let input = document.getElementById('input');
+  
+  let text = input.value;
+  let lines = text.split('\n');
+
+  lines.map(getLines);
+
+  console.log(metadata);
+  console.log(content);
+  /*
+  let output = document.getElementById('output');
+  output.contentDocument.open();
+  output.contentDocument.write(content);
+  output.contentDocument.close();
+  */
+}
