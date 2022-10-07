d94cbf8a6e329f4dde842c3a657094c8494ec332
SyenLxyz <syenlxyz@gmail.com>
2022-10-07 11:15:48 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,64 @@
+let generalSettings = {
+  'lyrics-only': null
+};
+
+let pageSettings = {
+  'margin-top': null,
+  'margin-bottom': null,
+  'margin-left': null,
+  'margin-right': null,
+  'orientation': null,
+  'paper-size': null,
+  'columns': null,
+  'column-space': null,
+  'header-space': null,
+  'footer-space': null,
+};
+
+let fontSettings = {
+  'title': {
+    'font-family': null,
+    'font-size': null,
+    'font-weight': null,
+    'font-style': null,
+    'text-decoration': null,
+    'line-height': null,
+    'color': null
+  },
+  'metadata': {
+    'font-family': null,
+    'font-size': null,
+    'font-weight': null,
+    'font-style': null,
+    'text-decoration': null,
+    'line-height': null,
+    'color': null
+  },
+  'heading': {
+    'font-family': null,
+    'font-size': null,
+    'font-weight': null,
+    'font-style': null,
+    'text-decoration': null,
+    'line-height': null,
+    'color': null
+  },
+  'chord': {
+    'font-family': null,
+    'font-size': null,
+    'font-weight': null,
+    'font-style': null,
+    'text-decoration': null,
+    'line-height': null,
+    'color': null
+  },
+  'lyric': {
+    'font-family': null,
+    'font-size': null,
+    'font-weight': null,
+    'font-style': null,
+    'text-decoration': null,
+    'line-height': null,
+    'color': null
+  }
+};
